221863db9f705f788f8934eca20f830a9588f6cd
DeveloGrater <Alligrater@outlook.com>
2022-08-29 09:47:30 +0800

    Update neofetchWindow.js

--- neofetchWindow.js
+++ neofetchWindow.js
@@ -20,7 +20,7 @@ class NeofetchWindow extends GsWindow{
       this.buffer.noFill();
       
       this.headPercentage = (this.lifespan * this.linespeed) % this.lineDst;
-      this.buffer.stroke(128, 0, 0);
+      this.buffer.stroke(96, 0, 0);
       let basePos;
       for(let i = 0; i < this.lineCount; i++){
         let fraction = 1 - i / this.lineCount;
@@ -51,14 +51,24 @@ class NeofetchWindow extends GsWindow{
         
         this.buffer.line(xCoord + halfWidth, this.sizeY, xCoord / zDistFar + halfWidth, this.sizeY - this.sizeY / zDistFar);
       }
+      
+      this.buffer.push();
       //line(
-      /*
-      textAlign(LEFT, TOP);
-      textStyle(BOLD);
-      textSize(12);
-      fill(255, 0, 0);
-      text("TEST", 4, 6);*/
+        this.buffer.textFont("consolas");
+        this.buffer.textAlign(CENTER, CENTER);
+        this.buffer.fill(255, 0, 0);
+        //this.buffer.textStyle(BOLD);
+        this.buffer.push();
+          this.buffer.textSize(16);
+          
+          this.buffer.text("GrateStation OS", this.sizeX * 0.5, this.sizeY * 0.5 - 8);
+        this.buffer.pop();
+        this.buffer.textSize(12);
+        this.buffer.text("By DeveloGrater", this.sizeX * 0.5, this.sizeY * 0.5 + 8);
+      this.buffer.pop();
     this.buffer.pop();
     image(this.buffer, 0, 0, this.sizeX, this.sizeY);
   }
+  
+ 
 }
